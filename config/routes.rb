c0576bd8a6350a18d0e644e537ea8efc602c88bc
Samuel Banya <sbanya@fastmail.com>
2022-11-04 17:36:43 -0500

    commit tree 81c3339158807bdaf982bf49150dcd591eadc12e

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Create a UsersController with a create method that responds to a POST /signup request.   
+  post "/signup", to: "users#create"
+
+  # Add a show method to your UsersController that responds to a GET /me request.   
+  get "/me", to: "users#show"
+
+  # Create a Sessions controller with a create action for logging in that responds to a POST /login request, and a destroy action for logging out that responds to a DELETE /logout request.
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
